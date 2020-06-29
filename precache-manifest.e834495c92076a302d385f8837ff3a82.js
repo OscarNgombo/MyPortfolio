051a48fece2020120243a5484eea4ae05630b657
Oscar Ngombo <oscarngombo@gmail.com>
2020-06-29 18:51:19 +0300

    Updates

---
+++ precache-manifest.e834495c92076a302d385f8837ff3a82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72a8574bacbddfdd31b9022268c02b22",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "b1f00ff4a4a8c2195b9e",
+    "url": "/MyPortfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e37f1e1a6789f9a952f9",
+    "url": "/MyPortfolio/static/css/main.13aa3287.chunk.css"
+  },
+  {
+    "revision": "b1f00ff4a4a8c2195b9e",
+    "url": "/MyPortfolio/static/js/2.1d8f284f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyPortfolio/static/js/2.1d8f284f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e37f1e1a6789f9a952f9",
+    "url": "/MyPortfolio/static/js/main.d6d27fdd.chunk.js"
+  },
+  {
+    "revision": "bcfb72ae68086a945547",
+    "url": "/MyPortfolio/static/js/runtime-main.e714cd19.js"
+  },
+  {
+    "revision": "c8dd92c5d7511e17b1e62cb598e926f7",
+    "url": "/MyPortfolio/static/media/bg.c8dd92c5.jpg"
+  },
+  {
+    "revision": "423ef456395e407ed650850829e5d02e",
+    "url": "/MyPortfolio/static/media/logo.423ef456.svg"
+  }
+]);
